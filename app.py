6dbcdaf41f4fd6d5c878e75b692274ab94876cf3
Jordan Bruce <jordanbruce@jordans-air-3.resource.campus.njit.edu>
2023-02-03 13:31:25 -0500

    setup database configurations

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
+import os
+
 from flask import Flask, render_template, request
 from flask_smorest import Api
+
+from db import db
+import models
+
 from resources.movie import blp as MovieBlueprint
 from resources.tv import blp as ShowBlueprint
 from resources.actor import blp as ActorBlueprint
 from resources.other import blp as OtherBlueprint
+from resources.login import blp as LoginBluePrint
 
 from secret import SECRET_API_KEY
 
-app = Flask(__name__)
+def create_app(db_url=None):
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app = Flask(__name__)
 
-api = Api(app)
-api.register_blueprint(MovieBlueprint)
-api.register_blueprint(ShowBlueprint)
-api.register_blueprint(ActorBlueprint)
-api.register_blueprint(OtherBlueprint)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    api = Api(app)
+
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(MovieBlueprint)
+    api.register_blueprint(ShowBlueprint)
+    api.register_blueprint(ActorBlueprint)
+    api.register_blueprint(OtherBlueprint)
+    api.register_blueprint(LoginBluePrint)
+
+    return app
 
 '''@app.route("/")
 def index():
